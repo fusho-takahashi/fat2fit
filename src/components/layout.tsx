eb001293ffcb8050a05838bdc08bd794b64601cf
fusho-takahashi <zenryaku@fusho-takahashi.me>
2021-03-05 18:58:47 +0900

    add ogp image

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -23,14 +23,9 @@ const Layout = ({
           name="description"
           content="Learn how to build a personal website using Next.js"
         />
-        {/* <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        /> */}
+        <meta property="og:image" content={`/ogp_image.png`} />
         <meta name="og:title" content={siteTitle} />
-        {/* <meta name="twitter:card" content="summary_large_image" /> */}
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       {home ? (
         <header className={styles.homeHeader}>
