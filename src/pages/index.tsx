4a771cdd562a46cf0b9a7ef0a1509f2993259f68
fusho-takahashi <zenryaku@fusho-takahashi.me>
2021-03-28 21:42:53 +0900

    add daily articles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ const Home = ({
         <title>{siteTitle}</title>
       </Head>
       <section className={styles.subHeading}>
-        <p>26日目: 103.4kg(-7.8kg)</p>
+        <p>28日目: 103.2kg(-8.0kg)</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <ul className={utilStyles.list}>
